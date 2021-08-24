aa995887e3c2b2dbbee5b2ca79bcbb7ef4a04e39
Mohammad Dabaja <mohammad.dabaja.edu@gmail.com>
2021-08-24 19:56:10 +0300

    Exercise 1 sol

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        List<Customer> customers = new List<Customer>()
+        {
+            new Customer()
+            {
+                Id = 1,
+                Name = "Mohammad"
+            },
+            new Customer()
+            {
+                Id = 2,
+                Name = "Ali"
+            }
+        };
+        
+        public IActionResult Index()
+        {
+            
+            return View(customers);
+        }
+
+        public IActionResult Details(int id)
+        {
+            if (id > customers.Count)
+            {
+                return new NotFoundResult();
+            }
+
+            return View(customers[id-1]);
+        }
+    }
+}
---
+++ Controllers/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customers = new List<Movie>()
+            {
+                new Movie()
+                {
+                    Id = 1,
+                    Name = "Superman"
+                },
+                new Movie()
+                {
+                    Id = 2,
+                    Name = "Spiderman"
+                }
+            };
+            return View(customers);
+        }
+    }
+}
